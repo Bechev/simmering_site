5ada79abe80a72b0c1f4a6fee26d124ccc41fbc9
bct <bertrand.chevalier@gmail.com>
2019-07-16 23:27:43 -0400

    Transform post message field from string to text and add character limit to 250 characters

--- db/migrate/20190717003228_create_posts.rb
+++ db/migrate/20190717003228_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :message
+      t.text :message
       t.integer :reshare
       t.integer :likes
       t.integer :user_id
