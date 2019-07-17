5bbd08ca8019ebee671bfae5b8064e5216a426ec
bct <bertrand.chevalier@gmail.com>
2019-07-16 22:12:05 -0400

    Reorganize controllers in API v1 folder

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "a user has a password" do
+    assert_equal false, User.new(username: "user").save
+    assert_equal true, User.new(username: "user", password: "password").save
+  end
+  
   test "a user has_many posts" do
     assert_equal 2, users(:user1).posts.count
   end
