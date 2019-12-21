b5f2beafe173184b95b45ec5d0f8fd293c235161
bct <bertrand.chevalier@gmail.com>
2019-12-21 12:50:02 -0500

    Formated AddToMealPlan window with form and placeholders

--- client/src/components/AddToMealPlan.js
+++ client/src/components/AddToMealPlan.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 // import {fetchMealPlan} from '../services/actions/mealplan.js'
 
 import './components.css'
@@ -8,31 +10,77 @@ import './components.css'
 
 class AddToMealPlan extends Component {
 
-  render() {
+    constructor(props) {
+        super(props);
 
-    return(
-        <div className="add_to_mealplan_blackground">
-            <div className="add_to_mealplan_window">
-                <div className="addRecipeToMealPlanDashboard">
-                    <div className="alt_button add_to_mealplan_button add_button" onClick={this.props.displayQuickAddToMealPlan}>
-                        Add
-                    </div>
-                    <div className="alt_button add_to_mealplan_button" onClick={this.props.displayQuickAddToMealPlan}>
-                        Cancel
-                    </div>
+        this.state = {
+            recipe_day: new Date(),
+            recipe_meal: ""
+        }
+        this.handleDateChange = this.handleDateChange.bind(this)
+        this.handleMealChange = this.handleMealChange.bind(this)
+        this.addRecipeToMealPlan = this.addRecipeToMealPlan.bind(this)
+    }
+
+    addRecipeToMealPlan() {
+        alert("day:" + this.state.recipe_day + "meal:" + this.state.recipe_meal)
+    }
+
+    // Stop the propagation to allow removing the AddToMealPlan window when cliking on the parent but not when clicking on the child.
+    stopPropagation(e) {
+        e.stopPropagation();
+    }
+
+    handleDateChange = date => {
+        this.setState({
+            startDate: date
+        });
+    };
+
+    handleMealChange(event) {
+        this.setState({
+            recipe_meal: event.target.value,
+        });
+
+    }
+
+    render() {
+
+        return (
+            <div className="add_to_mealplan_blackground" onClick={this.props.displayQuickAddToMealPlan} >
+                <div className="add_to_mealplan_window" onClick={this.stopPropagation}>
+                    {/* <div className="add_to_mealplan_form"> */}
+                        <form className="add_to_mealplan_form">
+                            <div className="date_picker add_to_mealplan_input">
+                                <label className='recipe_day_selection'> Day: </label>
+                                <br></br>
+                                <DatePicker className='input_field day_selection_input_field add_to_mealplan_input_field' dateFormat="yyyy/MM/dd" selected={this.state.recipe_day} onChange={this.handleDateChange} placeholder={this.state.recipe_day} />
+                            </div>
+                            <br></br>
+                            <div className="meal_selection add_to_mealplan_input">
+                                <label className='meal_selection'> Meal: </label>
+                                <br></br>
+                                <input className='input_field meal_selection_input_field add_to_mealplan_input_field' type="text" name="meal_value" value={this.state.recipe_meal} onChange={this.handleMealChange} placeholder="Meal" />
+                            </div>
+                            <br></br><br></br>
+                            <div className="add_to_mealplan_buttons">
+                                <input className='alt_button add_to_mealplan_button add_button' type="button" value="Add" onClick={this.addRecipeToMealPlan} />
+                                <input className='alt_button add_to_mealplan_button' type="button" value="Cancel" onClick={this.props.displayQuickAddToMealPlan} />
+                            </div>
+                        </form>
+                    {/* </div> */}
                 </div>
             </div>
-        </div>
         )
     }
 
 }
-  
+
 const mapStateToProps = (state, ownProps) => {
     return {
-      user: state.auth.user,
+        user: state.auth.user,
     }
-  }
+}
 
 const mapDispatchToProps = dispatch => {
     return {
