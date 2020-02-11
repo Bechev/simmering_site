5882d8232dc3354e201e4a8709094be8f3c888d7
bct <bertrand.chevalier@gmail.com>
2020-02-11 12:11:23 -0500

    Increased space between navbar text components

--- client/src/scenes/NavigationBar.js
+++ client/src/scenes/NavigationBar.js
@@ -44,25 +44,25 @@ class NavigationBar extends Component {
                     <React.Fragment>
                         <div className='navlinks'>
                             <div className="left-navbar">
-                                <NavLink className="navbar-link"
+                                <NavLink className="navbar-link icon"
                                 to="/"
                                 exact
                                 style={link}
                                 ><img src={Home} className='scene-icon' alt=""></img></NavLink>
 
-                                <NavLink className="navbar-link"
+                                <NavLink className="navbar-link text"
                                 to="/meal_plan"
                                 exact
                                 style={link}
                                 >MealPlan</NavLink>
         
-                                <NavLink className="navbar-link"
+                                <NavLink className="text navbar-link"
                                 to="/browse"
                                 exact
                                 style={link}
                                 >Browse</NavLink>
 
-                                <NavLink className="navbar-link"
+                                <NavLink className="text navbar-link"
                                 to="/blog"
                                 exact
                                 style={link}
@@ -70,13 +70,13 @@ class NavigationBar extends Component {
                             </div>
 
                             <div className="right-navbar">
-                                <NavLink className="navbar-link"
+                                <NavLink className="navbar-link icon"
                                 to="/mycart"
                                 exact
                                 style={link}
                                 ><img src={this.props.groceriesList.ingredients.length > 0 ? GroceriesList : EmptyGroceriesList} className='scene-icon' alt=""></img></NavLink>
 
-                                <NavLink className="navbar-link"
+                                <NavLink className="navbar-link icon"
                                 to="/profile"
                                 exact
                                 style={link}
@@ -93,13 +93,13 @@ class NavigationBar extends Component {
                 return(
                     <React.Fragment>
                         <div className="left-navbar">
-                            <NavLink className="navbar-link aligned-right"
+                            <NavLink className="navbar-link aligned-right text"
                             to="/signup"
                             exact
                             style={link}
                             >Sign up</NavLink>
                             
-                            <NavLink className="navbar-link aligned-right"
+                            <NavLink className="navbar-link aligned-right text"
                             to="/login"
                             exact
                             style={link}
